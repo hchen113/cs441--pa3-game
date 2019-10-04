1b4ed1be0ed6685ce09ad67abb558dc6fcf2ed0e
Henry Chen <hchen113@binghamton.edu>
2019-10-04 10:46:00 -0400

    player sprite and background image

--- cs441- pa3-game/GameViewController.swift
+++ cs441- pa3-game/GameViewController.swift
@@ -23,8 +23,8 @@ class GameViewController: UIViewController {
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+                //sceneNode.entities = scene.entities
+                //sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
@@ -40,6 +40,8 @@ class GameViewController: UIViewController {
                 }
             }
         }
+        
+        
     }
 
     override var shouldAutorotate: Bool {
